b9aa652039b3713b8606ad2e1581bc6813eb3019
Jason Cameron <jcameron@alley.com>
2023-03-30 13:53:07 -0400

    clean up

--- themes/twentytwentythree/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_filter('upload_mimes', 'add_yaml_mime_type');
-function add_yaml_mime_type($mime_types)
-{
-	$mime_types['yaml'] = 'application/x-yaml';
-	$mime_types['yml'] = 'text/yaml';
-	return $mime_types;
-}
